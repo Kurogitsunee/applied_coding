02489b01187efc9a537c0bb30e89ee5190632c2e
Kurogitsunee <lexa_tut_as@mail.ru>
2023-09-30 19:25:54 +0500

    added instructions for inputs

--- ФТ-220007 Фадеев1.py
+++ ФТ-220007 Фадеев1.py
@@ -1,4 +1,4 @@
 from time import localtime 
-name, age, city, country = input(), int(input()), input(), input()
+name, age, city, country = input('Write name: '), int(input('Write age: ')), input('Write city: '), input('Write country: ')
 born_year = localtime().tm_year - age
 print(f"Уважаемый {name}!\nНа сегодняшний день Вы проживаете в стране {country}, в городе {city}, и вы родились в {born_year} году.")
